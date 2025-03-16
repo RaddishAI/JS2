501222bb2d0fb29e60f22d614d666692966e723b
RaddishAI <petter.arbeid@gmail.com>
2025-03-16 19:34:41 +0100

    fixed up global dofetch for login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,47 +1,39 @@
-/* console.log("Script Loaded");
- */
-import { LOGIN_API_URL } from "../../scripts/constants.js";
+import { LOGIN_API_URL } from '../../scripts/constants.js';
+import { doFetch } from '../../utils/doFetch.js';
 
 const loginForm = document.getElementById('login-form');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 async function loginUser(userData) {
-    try {
-        const fetchOptions = {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        };
-        const response = await fetch (LOGIN_API_URL, fetchOptions)
-        const json = await response.json();
-        const accessToken = json.data.accessToken;
-        const userDetails = {
-            name: json.data.name,
-            email: json.data.email,
-        };
+  try {
+    const fetchOptions = {
+      method: 'POST',
+      body: JSON.stringify(userData),
+    };
+    const response = await doFetch(LOGIN_API_URL, fetchOptions);
+    const accessToken = response.data.accessToken;
+    const userDetails = {
+      name: response.data.name,
+      email: response.data.email,
+    };
 
-        localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('userDetails', JSON.stringify(userDetails));
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('userDetails', JSON.stringify(userDetails));
 
-        setTimeout(function() {
-            window.location.href = '/';
-        }, 2000);
-    } catch (error) {
-        console.log(error);        
-    }
-
-
+    setTimeout(function () {
+      window.location.href = '/';
+    }, 2000);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-/*     console.log ('submit'); */
-    const userData = {
-      email: emailInput.value,
-      password: passwordInput.value,
-    };
-    loginUser(userData);
-  });
+  event.preventDefault();
+  const userData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+  loginUser(userData);
+});
