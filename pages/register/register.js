6483bdb65094d80fb70e756b5c1f2d2b083fb33e
RaddishAI <petter.arbeid@gmail.com>
2025-03-16 15:51:16 +0100

    added registerfunction with placeholder. 201

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,13 +1,45 @@
+// base url: https://v2.api.noroff.dev/
+// register part /auth/register
+
+const BASE_API_URL = 'https://v2.api.noroff.dev';
+
+const REGISTER_API_URL = BASE_API_URL + '/auth/register';
+
 const registerForm = document.getElementById('register-form');
-const usernameInput = document.getElementById('username');
+const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
-function registerUser(userData) {
+async function registerUser(userData) {
     console.log ('Register user', userData);
+
+    try {
+        const fetchOptions = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        };
+
+        const response = await fetch(REGISTER_API_URL, fetchOptions);
+        const json = await response.json();
+    } catch (error) {
+        //
+    }
 }
 
-function main(){
+registerForm.addEventListener('submit', function (event){
+    event.preventDefault();
+    const userData = {
+        name: nameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+    }
+    registerUser(userData);
+});
+
+/* function main(){
     const userData = {
         username: usernameInput.value,
         email: emailInput.value,
@@ -16,4 +48,4 @@ function main(){
    registerUser(userData); 
 }
 
-main();
+main(); */
